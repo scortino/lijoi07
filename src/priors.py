6b0eb3c2884c9dab01ea3037f381a6485ed6be1c
scortino <stefanocortinovis17@gmail.com>
2021-12-24 19:14:40 +0100

    Implement DirichletProcess

--- src/priors.py
+++ src/priors.py
@@ -1,4 +1,6 @@
-from utils import generalized_factorial, noncentral_generalized_factorial
+from scipy.special import comb
+
+from .utils import rising_factorial, generalized_factorial, noncentral_generalized_factorial, stirling
 
 class GibbsType:
     def __init__(self, sigma, V):
@@ -18,3 +20,26 @@ class GibbsType:
         for k in range(m + 1):
             s += self.V(n + m + 1, j + k + 1) * self.nC(m, k, -n + j * self.sigma) / (self.sigma**k)
         return s / self.V(n, j)
+
+class DirichletProcess(GibbsType):
+    def __init__(self, theta):
+        self.theta = theta
+        super().__init__(0, lambda n, k: (theta**k) / rising_factorial(theta, n))
+
+    def prior_prob_K(self, n, k):
+        return self.theta**k * stirling(n, k) / rising_factorial(self.theta, n)
+
+    def posterior_prob_K(self, m, k, n, j):
+        s = 0
+        for l in range(k, m + 1):
+            s += comb(m, l) * stirling(l, k) * rising_factorial(n, m - l)
+        return s * self.theta**k * rising_factorial(self.theta, n) / rising_factorial(self.theta, n + m)
+    
+    def Dhat_m(self, m, n, j):
+        s1 = 0
+        for k in range(m + 1):
+            s2 = 0 
+            for l in range(k, m + 1):
+                s2 += comb(m, l) * stirling(l, k) * rising_factorial(n, m - l)
+            s1 += self.theta**k * s2
+        return s1 * self.theta / rising_factorial(self.theta + n, m + 1)
