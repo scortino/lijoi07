ca0306264ab51e8c0f570139a8addbdf9b8f7003
scortino <stefanocortinovis17@gmail.com>
2022-01-11 00:12:04 +0100

    Validate function arguments

--- src/priors.jl
+++ src/priors.jl
@@ -8,11 +8,25 @@ struct DirichletProcess
 end
 
 function prior_probability(prior::DirichletProcess, n::Integer, k::Integer)
+    if n <= 0
+        throw(DomainError(n, "n must be a positive integer"))
+    elseif !(1 <= k <= n)
+        throw(DomainError(k, "k must be between 1 and n"))
+    end
+
     prior.θ^k * stirling(n, k) / rising_factorial(prior.θ, n)
 end
 
 # Notice it doesn't depend on j
 function posterior_probability(prior::DirichletProcess, m::Integer, k::Integer, n::Integer)
+    if m <= 0
+        throw(DomainError(m, "m must be a positive integer"))
+    elseif !(0 <= k <= m)
+        throw(DomainError(k, "k must be between 1 and m"))
+    elseif n <= 0
+        throw(DomainError(n, "n must be a positive integer"))
+    end
+
     s = sum(
         binomial(m, l) * stirling(l, k) * rising_factorial(n, m - l) for l = k:m;
         init = 0,
@@ -26,6 +40,12 @@ struct PoissonDirichletProcess
 end
 
 function prior_probability(prior::PoissonDirichletProcess, n::Integer, k::Integer)
+    if n <= 0
+        throw(DomainError(n, "n must be a positive integer"))
+    elseif !(1 <= k <= n)
+        throw(DomainError(k, "k must be between 1 and n"))
+    end
+
     p = prod(prior.θ + i * prior.σ for i = 1:(k-1); init = 1)
     p / (prior.σ^k * rising_factorial(prior.θ + 1, n - 1)) *
     generalized_factorial(n, k, prior.σ)
@@ -38,6 +58,16 @@ function posterior_probability(
     n::Integer,
     j::Integer,
 )
+    if m <= 0
+        throw(DomainError(m, "m must be a positive integer"))
+    elseif !(0 <= k <= m)
+        throw(DomainError(k, "k must be between 1 and m"))
+    elseif n <= 0
+        throw(DomainError(n, "n must be a positive integer"))
+    elseif !(1 <= j <= n)
+        throw(DomainError(j, "j must be between 1 and n"))
+    end
+
     p = prod(prior.θ + i * prior.σ for i = j:(j+k-1); init = 1)
     rising_factorial(prior.θ + 1, n - 1) / rising_factorial(prior.θ + 1, n + m - 1) * p /
     prior.σ^k * noncentral_generalized_factorial(m, k, prior.σ, -n + j * prior.σ)
@@ -50,6 +80,12 @@ end
 
 # When gamma takes two parameters, it evaluates to incomplete gamma function
 function prior_probability(prior::NormalizedIGProcess, n::Integer, k::Integer)
+    if n <= 0
+        throw(DomainError(n, "n must be a positive integer"))
+    elseif !(1 <= k <= n)
+        throw(DomainError(k, "k must be between 1 and n"))
+    end
+
     s = sum(
         binomial(n - 1, i) * (-prior.θ^2)^(-i) * gamma(k + 2 + 2i - 2n, prior.θ) for
         i = 0:(n-1);
@@ -67,6 +103,16 @@ function posterior_probability(
     n::Integer,
     j::Integer,
 )
+    if m <= 0
+        throw(DomainError(m, "m must be a positive integer"))
+    elseif !(1 <= k <= m)
+        throw(DomainError(k, "k must be between 1 and m"))
+    elseif n <= 0
+        throw(DomainError(n, "n must be a positive integer"))
+    elseif !(1 <= j <= n)
+        throw(DomainError(j, "j must be between 1 and n"))
+    end
+
     s1 = s2 = s3 = 0
     for i = 0:(n+m-1)
         s1 +=
--- src/utils.jl
+++ src/utils.jl
@@ -4,12 +4,20 @@ using SpecialFunctions
 
 # Rising factorial
 function rising_factorial(a::Real, n::Integer)
+    if n < 0
+        throw(DomainError(n, "n must be a positive integer"))
+    end
+
     prod(a + j for j = 0:(n-1); init = 1)
 end
 
 # Central generalized factorial coefficient
 function generalized_factorial(n::Integer, k::Integer, σ::AbstractFloat)
-    if n == k == 0
+    if n < 0
+        throw(DomainError(n, "n must be a non-negative integer"))
+    elseif k < 0
+        throw(DomainError(k, "k must be a non-negative integer"))
+    elseif n == k == 0
         return 1
     end
 
@@ -26,6 +34,12 @@ function noncentral_generalized_factorial(
     σ::AbstractFloat,
     γ::AbstractFloat,
 )
+    if n < 0
+        throw(DomainError(n, "n must be a non-negative integer"))
+    elseif k < 0
+        throw(DomainError(k, "k must be a non-negative integer"))
+    end
+
     sum(
         binomial(n, j) * generalized_factorial(j, k, σ) * rising_factorial(-γ, n - j) for
         j = k:n;
@@ -37,7 +51,11 @@ end
 # https://en.wikipedia.org/wiki/Stirling_numbers_of_the_first_kind
 # n>=0, k>=0
 @memoize LRU{Tuple{Any,Any},Any}(maxsize = 128) function stirling(n::Integer, k::Integer)
-    if n == k == 0
+    if n < 0
+        throw(DomainError(n, "n must be a non-negative integer"))
+    elseif k < 0
+        throw(DomainError(k, "k must be a non-negative integer"))
+    elseif n == k == 0
         return 1
     elseif (n == 0) || (k == 0)
         return 0
